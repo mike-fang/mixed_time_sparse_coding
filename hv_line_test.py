2c5417ee2f01af5a5fdb1361a222017deafc60df
Michael Fang <Michael Fang>
2019-08-07 15:43:47 -0700

    hv sort of converged

--- hv_line_test.py
+++ hv_line_test.py
@@ -7,9 +7,9 @@ from matplotlib import animation
 from visualization import show_img_evo
 
 # Define energy
-l1 = .3
-l0 = .3
-sigma = .5
+l1 = .5
+l0 = .6
+sigma = .2
 energy = Energy_L0(sigma, l0, l1, positive=True)
 
 #Define loader
@@ -23,14 +23,14 @@ n_sparse = 10
 n_dim = int(H * W)
 params = {
         'tau_s': 1e2,
-        'tau_x': 5e3,
+        'tau_x': 3e3,
         'tau_A': 1e5,
-        'mu_s': .05,
-        'mu_A': .00,
+        'mu_s': .1,
+        'mu_A': .1,
         }
 
 # Time range
-T_RANGE = 1e4
+T_RANGE = 2e6
 T_STEPS = int(T_RANGE)
 tspan = np.linspace(0, T_RANGE, T_STEPS, endpoint=False)
 
@@ -47,11 +47,15 @@ s0 = np.zeros((n_batch, n_sparse))
 pA0 = np.zeros_like(A0)
 ps0 = np.zeros_like(s0)
 
-def train_mtsc(f_name=None, n_frames=1000):
+def train_mtsc(f_name=None, n_frames=1000, good_init=False):
     skip = int(max(len(tspan)//n_frames, 1))
     out_t = tspan[::skip]
     mtsc = MixedTimeSC(n_dim, n_sparse, energy, im_shape=(H, W), **params)
-    soln_dict = mtsc.train(loader, tspan, out_t=out_t, init_sA=(s0, A0, ps0, pA0))
+    if good_init:
+        init = (s0, A0, ps0, pA0)
+    else:
+        init = None
+    soln_dict = mtsc.train(loader, tspan, out_t=out_t, init_sA=init)
     soln = save_model(mtsc, soln_dict, loader, f_name, overwrite=True)
     return soln
 
@@ -65,12 +69,13 @@ def train_dsc():
     soln = Solutions(soln_dict, im_shape=(H, W))
     soln.save(f_name='./results/hv_line_dsc.soln')
 
-train_mtsc('./results/hv_mtsc_momentum', n_frames=10000)
-soln = Solutions_H5.load_h5('./results/hv_mtsc_momentum/soln.h5')
+out_dir = './results/hv_mtsc_momentum_l0_08'
+#soln = train_mtsc(out_dir, n_frames=int(1e5))
+soln = Solutions_H5.load_h5(os.path.join(out_dir, 'soln.h5'))
 reshaped_params = soln.get_reshaped_params()
 A = reshaped_params['A']
 R = reshaped_params['R']
 X = reshaped_params['X']
 
 XRA = np.concatenate((X, R, A), axis=1)
-show_img_evo(XRA, ratio = 10/3, n_frames=100)
+show_img_evo(XRA, ratio = 10/3, n_frames=1000)
