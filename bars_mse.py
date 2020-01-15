normalization fix

bars_mse.py
@@ -5,7 +5,7 @@ import matplotlib.pylab as plt
 import os.path
 import seaborn as sns
 
-def plot_exp(exp, color, n_bins=100, start_frac=0, end_frac=1, alpha=.3):
+def plot_mse(exp, color, n_bins=100, start_frac=0, end_frac=1, alpha=.3, q=0.2):
     base_dir = f'bars_{exp}'
     dir_path = get_timestamped_dir(load=True, base_dir=base_dir)
     soln = h5py.File(os.path.join(dir_path, 'soln.h5'))
@@ -24,20 +24,55 @@ def plot_exp(exp, color, n_bins=100, start_frac=0, end_frac=1, alpha=.3):
     for t0, t1 in zip(T0, T1):
         inter = (t0 <= t) & (t < t1)
         mse_med.append(np.quantile(mse[inter], 0.5))
-        mse_low.append(np.quantile(mse[inter], 0.2))
-        mse_high.append(np.quantile(mse[inter], 0.8))
-
-
-
+        mse_low.append(np.quantile(mse[inter], q))
+        mse_high.append(np.quantile(mse[inter], 1-q))
 
     plt.plot(T0, -np.log(mse_med), color=color, label=exp)
     plt.fill_between(T0, -np.log(mse_high), -np.log(mse_low), color=color, alpha=alpha)
 
-START = .001
-END = .3
-plot_exp('dsc', 'b', start_frac=START, end_frac=END)
-plot_exp('ctsc', 'g', start_frac=START, end_frac=END)
-plot_exp('asynch', 'r', start_frac=START, end_frac=END, alpha=.2)
-plot_exp('lsc', 'm', start_frac=START, end_frac=END, alpha=.2)
-plt.legend()
-plt.show()
+def plot_energy(exp, color):
+    base_dir = f'bars_{exp}'
+    dir_path = get_timestamped_dir(load=True, base_dir=base_dir)
+    soln = h5py.File(os.path.join(dir_path, 'soln.h5'))
+    print(soln['A'])
+    t = soln['t'][:]
+    A = soln['A'][:]
+    A = A.reshape((-1, 8, 8, 16))
+    A0 = A[-1].copy()
+    A0[A0 < .1] = 0
+    A0[A0 > .1] = 8**(-0.5)
+
+    cos_sim = np.sum(A * A0, axis=(1, 2))
+    mean_cs = cos_sim.mean(axis=1)
+    plt.plot(t, mean_cs, color=color, label=exp)
+    plt.ylim(0, 1)
+
+
+colors = {'dsc': 'black', 'ctsc': 'g', 'asynch': 'r', 'lsc': 'blue'}
+
+if True:
+    Q = .2
+    fig = plt.figure(figsize=(8, 3))
+    for exp in colors:
+        c = colors[exp]
+        plot_mse(exp, c, q=Q, n_bins=50, start_frac=0, end_frac=1, alpha=.2)
+    plt.legend(loc=4)
+    plt.ylabel('Reconstr. NL-MSE')
+    plt.xlabel('Time')
+    plt.xlim(0, 1e5)
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.savefig('./figures/bars_mse.pdf')
+    plt.show()
+else:
+
+    fig = plt.figure(figsize=(8, 3))
+    for exp in colors:
+        c = colors[exp]
+        plot_energy(exp, c)
+    plt.ylabel('Dict. Cosine Similarity')
+    plt.xlabel('Time')
+    plt.xlim(0, 1e5)
+    plt.legend(loc=4)
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.savefig('./figures/bars_cosine.pdf')
+    plt.show()

ctsc.py
@@ -32,7 +32,13 @@ def get_time_interval(tspan, N=None, dt=None):
         idx = np.argmin(np.abs(tspan - n*dt))
         interval[idx] = True
     return interval
-def get_timestamped_dir(load=False, base_dir=None):
+def get_timestamped_dir(load=False, base_dir=None, dir_name=None):
+    if dir_name is not None:
+        dir_name = os.path.join(FILE_DIR, 'results', base_dir, dir_name)
+        if not load:
+            os.makedirs(dir_name)
+        return dir_name
+
     if base_dir is None:
         base_dir = 'tmp'
     if load:

l_tiles.py
@@ -1,7 +1,7 @@
 import torch as th
 import numpy as np
 from ctsc import *
-from loaders import BarsLoader
+from loaders import LTileLoader
 from visualization import show_img_XRA, show_batch_img
 import matplotlib.pylab as plt
 from soln_analysis import SolnAnalysis
@@ -18,7 +18,6 @@ def plot_dict():
     #fig.suptitle('Bars Dictionary')
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
-
 def plot_samples(pi=.3, l1=1, sigma=0):
     loader = BarsLoader(H, W, 16, l1=l1, p=pi, sigma=sigma, numpy=True)
     x = loader().reshape((-1, 8, 8))
@@ -33,23 +32,22 @@ def plot_samples(pi=.3, l1=1, sigma=0):
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
 # Define loader
-H = W = 8
+H = W = 4
 N_DIM = H * W
-N_BATCH = 2 * (H * W)
-N_DICT = H + W
+N_BATCH = 4 * (H * W)
 PI = 0.3
 SIGMA = .5
 LARGE = False
 N_S = 100
 L1 = 1
-loader = BarsLoader(H, W, N_BATCH, p=PI, sigma=SIGMA, l1=L1)
-EXP = 'ctsc'
+loader = LTileLoader(H, W, N_BATCH, p=PI, sigma=SIGMA, l1=L1)
+N_DICT = loader.n_dict
 
 # DSC params
 if not LARGE:
     N_A = 1000
     N_S = N_S
-    ETA_A = 0.03
+    ETA_A = 0.05
     ETA_S = 0.05
 else:
     N_A = 10
@@ -69,16 +67,16 @@ model_params = dict(
         sigma=SIGMA,
         )
 
+EXP = 'ctsc'
 LOAD = False
 assert EXP in ['dsc', 'ctsc', 'asynch', 'lsc']
 if LARGE:
-    base_dir = f'bars_large_step_{EXP}'
+    base_dir = f'ltiles_large_step_{EXP}'
 else:
-    base_dir = f'bars_{EXP}'
+    base_dir = f'ltiles_{EXP}'
 
 # Define model, solver
 model = CTSCModel(**model_params)
-#model.A.data = loader.bases.T
 #model.A.data = th.tensor(np.load('./A0.npy'))
 solver_params = CTSCSolver.get_dsc(model, n_A=N_A, n_s=N_S, eta_A=ETA_A, eta_s=ETA_S, return_params=True)
 if EXP == 'dsc':
@@ -116,3 +114,4 @@ A = soln['A'][:]
 
 show_img_XRA(X, R, A, n_frames=1e2, img_shape=(H, W))
 plt.show()
+

loaders.py
@@ -64,7 +64,8 @@ class StarLoader_(Loader):
         return X
 
 class BarsLoader:
-    def __init__(self, H, W, n_batch, p=0.1, positive=False, test=False, numpy=False, sigma=1):
+    def __init__(self, H, W, n_batch, p=0.1, positive=True, test=False, numpy=False, sigma=1, l1=1):
+        self.l1 = l1
         self.H = H
         self.W = W
         self.im_shape = (H, W)
@@ -83,15 +84,12 @@ class BarsLoader:
             test = True
         if n_batch is None:
             n_batch = self.n_batch
-        S = th.Tensor(n_batch, self.W + self.H).bernoulli_(self.p)
-        multiplier = th.Tensor(n_batch, self.W + self.H).uniform_()
-        if self.positive:
-            multiplier -= 0.5
-        S *= multiplier
         if test:
-            d_min = min(n_batch, self.W + self.H)
+            d_min = min(n_batch, self.n_dict)
             S *= 0
             S[:d_min, :d_min] = th.eye(d_min)
+        else:
+            S = self.get_coeff(n_batch=n_batch)
 
         batch = S @ self.bases
         noise = th.Tensor(batch.shape)
@@ -105,17 +103,32 @@ class BarsLoader:
             return batch.reshape((n_batch, self.H, self.W))
         else:
             return batch
+    def get_coeff(self, n_batch=None):
+        if n_batch is None:
+            n_batch = self.n_batch
+        S = th.Tensor(n_batch, self.n_dict).bernoulli_(self.p)
+        coeff = np.abs(np.random.laplace(0, scale=1/self.l1, size=(n_batch, self.n_dict)))
+        multiplier = th.tensor(coeff)
+        S *= multiplier
+        if not self.positive:
+            flip = th.Tensor(n_batch, self.n_dict).bernoulli_(0.5) * 2 - 1
+            S *= flip
+        return S
     def set_bases(self, flatten=True):
-        bases = th.zeros((self.H + self.W, self.H, self.H))
+        bases = th.zeros((self.H + self.W, self.H, self.W))
         for i in range(self.H):
-            bases[i, i] = 1
+            bases[i, i] = self.W**(-0.5)
         for i in range(self.W):
-            bases[self.H + i, :, i] = 1
+            bases[self.H + i, :, i] = self.H**(-0.5)
+
         if flatten:
             self.bases = bases.reshape((self.H + self.W, -1))
         else:
             self.bases = bases
         return self.bases
+    @property
+    def n_dict(self):
+        return len(self.bases)
     def __call__(self, n_batch=None):
         return self.get_batch(n_batch=n_batch)
     def __repr__(self):
@@ -124,6 +137,92 @@ class BarsLoader:
         desc += f'n_batch: {self.n_batch}\n'
         desc += f'p: {self.p}'
         return desc
+
+class DominosLoader(BarsLoader):
+    def __init__(self, H, W, n_batch, p=0.1, positive=True, test=False, numpy=False, sigma=1, l1=1):
+        super().__init__(H, W, n_batch, p, positive, test, numpy, sigma, l1)
+        self.set_bases()
+    def set_bases(self, flatten=True):
+        #bases = th.zeros((self.n_dict, self.H, self.W))
+        bases = []
+        for x in range(self.W):
+            for y in range(self.H):
+                if x < self.W - 1:
+                    basis = th.zeros((H, W))
+                    basis[y, x] = 1
+                    basis[y, x + 1] = 1
+                    bases.append(basis)
+                if y < self.H - 1:
+                    basis = th.zeros((H, W))
+                    basis[y, x] = 1
+                    basis[y + 1, x] = 1
+                    bases.append(basis)
+        self.bases = th.stack(bases)
+        if flatten:
+            self.bases = self.bases.reshape((self.n_dict, -1))
+        else:
+            self.bases = self.bases
+        return self.bases
+
+class LTileLoader(BarsLoader):
+    def __init__(self, H, W, n_batch, p=0.1, positive=True, test=False, numpy=False, sigma=1, l1=1):
+        super().__init__(H, W, n_batch, p, positive, test, numpy, sigma, l1)
+        self.set_bases()
+    def set_bases(self, flatten=True):
+        #bases = th.zeros((self.n_dict, self.H, self.W))
+        bases = []
+        for x in range(self.W - 1):
+            for y in range(self.H - 1):
+                square = th.zeros((self.H, self.W))
+                square[x, y] = 1
+                square[x + 1, y] = 1
+                square[x, y + 1] = 1
+                square[x + 1, y + 1] = 1
+                for dx in (0, 1):
+                    for dy in (0, 1):
+                        basis = square.clone()
+                        basis[x + dx, y + dy] = 0
+                        bases.append(basis)
+        self.bases = th.stack(bases)
+        if flatten:
+            self.bases = self.bases.reshape((self.n_dict, -1))
+        else:
+            self.bases = self.bases
+        return self.bases
+
+def paint_tile(tile_shape, x, y, coords):
+    basis = th.zeros(tile_shape)
+    for dy, dx in coords:
+        basis[y + dy, x + dx] = 1
+    return basis
+
+class TTileLoader(BarsLoader):
+    def __init__(self, H, W, n_batch, p=0.1, positive=True, test=False, numpy=False, sigma=1, l1=1):
+        super().__init__(H, W, n_batch, p, positive, test, numpy, sigma, l1)
+        self.set_bases()
+    def set_bases(self, flatten=True):
+        #bases = th.zeros((self.n_dict, self.H, self.W))
+        bases = []
+        for x in range(self.W - 1):
+            for y in range(self.H - 1):
+                if y < self.H - 2:
+                    basis = paint_tile((self.H, self.W), x, y, [(0,0), (1,0), (2,0), (1,1)])
+                    bases.append(basis)
+                    basis = paint_tile((self.H, self.W), x, y, [(0,1), (1,1), (2,1), (1,0)])
+                    bases.append(basis)
+                if x < self.W - 2:
+                    basis = paint_tile((self.H, self.W), x, y, [(0,0), (0,1), (0,2), (1,1)])
+                    bases.append(basis)
+                    basis = paint_tile((self.H, self.W), x, y, [(1,0), (1,1), (1,2), (0,1)])
+                    bases.append(basis)
+
+
+        self.bases = th.stack(bases)
+        if flatten:
+            self.bases = self.bases.reshape((self.n_dict, -1))
+        else:
+            self.bases = self.bases
+        return self.bases
 
 class SparseSampler():
     def __init__(self, A, n_batch, pi=0.2, l1=.2, sigma=0, positive=True, coeff='exp'):
@@ -276,11 +375,18 @@ class GaussainSampler():
             return self.sample_buffer(n_batch)
 
 if __name__ == '__main__':
-    H = W = 16
+
+    H = W = 8
     n_batch = 16
     vh_sampler = VanHaterenSampler(H, W, n_batch, buffer_size=1e3)
+    loader = BarsLoader(H, W, n_batch, l1=1, p=0.1, sigma=0.1)
+
+    plt.imshow(loader.bases[0].reshape(H, W))
+    plt.show()
+    assert False
     t0 = time()
-    ims = vh_sampler(16).reshape((16, H, W))
+    print(loader.bases.shape)
+    ims = loader(16).reshape((16, H, W))
     print(time() - t0)
     for n, im in enumerate(ims):
         plt.subplot(4, 4, n+1)

ltiles_sparsity.py
@@ -10,23 +10,22 @@ def plot_density(analysis, name, color, thresh=2e-2):
     time = analysis.time
     plt.plot(time, mean_nz, 'k', color=color, label=name)
 
-def plot_nz_distr():
+if __name__ == '__main__':
     colors = ['b', 'g', 'r', 'k']
     exps = ['lsc', 'ctsc', 'asynch', 'dsc']
     #fig = plt.figure(figsize=(8, 3))
     for c, exp in zip(colors, exps):
-        base_dir = f'bars_{exp}'
+        base_dir = f'ttiles_{exp}'
         dir_path = get_timestamped_dir(load=True, base_dir=base_dir)
-        print(dir_path)
         #dir_path = get_timestamped_dir(load=True, base_dir=base_dir, dir_name='bars_complete')
         analysis = SolnAnalysis(dir_path)
         analysis.zero_coeffs(thresh=2e-2)
-        analysis.plot_nz_hist(last_frac=.2, s_max=6, title=None, n_bins=100, log=False, eps_s=1e-5, ylim='auto')
+        analysis.plot_nz_hist(last_frac=.3, s_max=6, title=exp, n_bins=100, log=False, eps_s=1e-5, ylim='auto')
 
-        plt.savefig(f'./figures/bars_distr_{exp}.pdf')
+        #plt.savefig('./figures/ltiles_distr_complete.pdf')
         plt.show()
 
-if __name__ == '__main__':
+        assert False
     colors = ['k', 'g', 'r', 'b']
     exps = ['dsc', 'ctsc', 'asynch', 'lsc']
     fig = plt.figure(figsize=(8, 3))
@@ -37,10 +36,11 @@ if __name__ == '__main__':
         plot_density(analysis, name=exp, color=c, thresh=1e-2)
     time = analysis.time
     plt.plot(time, np.ones_like(time) * 0.3, 'm--', label=r'$\pi = 0.3$')
-    plt.xlim(0, 1e4)
+    plt.xlim(0, 5e4)
     plt.legend()
     plt.ylabel('Density')
     plt.xlabel('Time')
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
     plt.savefig(f'./figures/bars_density.pdf')
     plt.show()
+

soln_analysis.py
@@ -93,33 +93,32 @@ class SolnAnalysis:
             mean_nz[:smoothing] = mean_nz[smoothing]
         return mean_nz
     def plot_nz_hist(self, title='', last_frac=0.1, s_max=3, eps_s=1e-5, log=True, n_bins=100, ylim=None):
-        pi = self.pi
         l1 = self.l1
-        u0 = -np.log(pi) / l1
 
         S_soln = self.soln['s'][:]
         N_S, _, _ = S_soln.shape
+        print(S_soln.shape)
         S_converged = S_soln[-int(N_S*last_frac):].flatten()
 
         l0_sparsity = (S_converged < eps_s).mean()
 
-        bins = np.linspace(0, s_max / l1)
-        bins[1] = eps_s
+        bins = np.linspace(eps_s, s_max / l1)
+        #bins[1] = eps_s
         #bins = np.insert(bins, 1, eps_s)
         plt.hist(S_converged.flatten(), bins=bins, density=True, fc='grey', ec='black', label='Prob. Distr.')
-        prob_expected = l1 * np.exp(-l1 * (bins + u0))
-        plt.plot(bins, prob_expected, 'r--', label=r'$P_S(s) = \frac{\pi}{\lambda}e^{- \lambda \cdot s}$')
+        prob_expected = l1 * np.exp(-l1 * (bins))
+        plt.plot(bins, prob_expected, 'r--', label=r'$P_S(s) = \lambda e^{- \lambda \cdot s}$')
         if log:
             plt.yscale('log')
         if ylim == 'auto':
-            plt.ylim(0, 1.2 * l1 * pi)
+            plt.ylim(0, 1.2 * l1)
         else:
             plt.ylim(ylim)
         plt.xlim(eps_s, bins[-1])
         plt.ylabel(rf'Distr. of Nonzero Coefficients')
         plt.xlabel(rf'Coeff. Value ($s$); Emperical $P(s > \epsilon_s) = {1-l0_sparsity:.2f}$')
 
-        title += rf' $(\pi = {pi:.2f}, \lambda_1 = {l1:.2f})$'
+        #title += rf' $(\lambda_1 = {l1:.2f})$'
         plt.title(title)
         plt.legend()
     def show_hist_evo(self, t_inter, thresh = 1e-1):